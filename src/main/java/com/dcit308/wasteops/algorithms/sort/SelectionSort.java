d32780a6a8ac7495a1facd928546b3d0f34bb154
AduMintah <mmadu001@st.ug.edu.gh>
2026-08-17 02:56:15 +0000

    feat: implement SelectionSort algorithm and RedBlackTree structure

--- src/main/java/com/dcit308/wasteops/algorithms/sort/SelectionSort.java
+++ src/main/java/com/dcit308/wasteops/algorithms/sort/SelectionSort.java
@@ -11,6 +11,20 @@ public class SelectionSort<T extends Comparable<T>> implements SortAlgorithm<T> 
 
     @Override
     public void sort(T[] array) {
-        throw new UnsupportedOperationException("TODO: Issue #7 \u2014 implement SelectionSort, in place, ascending order.");
+        if (array == null || array.length <=1) {
+            return;
+        }
+        int n = array.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j=i+1; j<n; j++) {
+                if (array[j].compareTo(array[minIndex]) <0 ){minIndex = j;}
+            }
+            if (minIndex != i) {
+                T temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+        }
     }
+}
 }
